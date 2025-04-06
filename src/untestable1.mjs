23f25beb7b248bc27fe3067d11a48fff23004f17
anastat <anastasiia.tikhonova@mail.com>
2025-04-06 18:11:32 +0300

    Changes for the first untestable.

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,5 +1,8 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
+/**
+ * Imposible to test as now always different
+ */
 export function daysUntilChristmas() {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -8,5 +11,15 @@ export function daysUntilChristmas() {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
   }
   const diffMillis = christmasDay.getTime() - today.getTime();
+  return Math.floor(diffMillis / millisPerDay);
+}
+
+export function daysUntilChristmasCopy(date) {
+  const newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  const christmasDay = new Date(date.getFullYear(), 12 - 1, 25);
+  if (newDate.getTime() > christmasDay.getTime()) {
+    christmasDay.setFullYear(new Date().getFullYear() + 1);
+  }
+  const diffMillis = christmasDay.getTime() - newDate.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,10 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmasCopy } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("275 days until Christmas from 25.3.2025", () => {
+    const dateToTest = new Date("2025-03-25T03:24:00");
+    expect(daysUntilChristmasCopy(dateToTest)).to.be.equal(275);
   });
 });
