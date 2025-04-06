ae7844149577a16582bedf9762f15f6e9e74b3f8
anastat <anastasiia.tikhonova@mail.com>
2025-04-06 18:12:08 +0300

    Changes for untestable 2.

--- src/untestable2.mjs
+++ src/untestable2.mjs
@@ -4,9 +4,28 @@ function diceRoll() {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
+/**
+ * Hard to test random values. Especially as result differs if die1 and die2 are equal.
+ */
 export function diceHandValue() {
   const die1 = diceRoll();
   const die2 = diceRoll();
+  if (die1 === die2) {
+    // one pair
+    return 100 + die1;
+  } else {
+    // high die
+    return Math.max(die1, die2);
+  }
+}
+
+function diceRollCopy(min = 1, max = 6) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+export function diceHandValueCopy(min, max) {
+  const die1 = diceRollCopy(min, max);
+  const die2 = diceRollCopy(min, min);
   if (die1 === die2) {
     // one pair
     return 100 + die1;
--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,10 +1,19 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/untestable2.mjs";
+import { diceHandValueCopy } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+  test("the result is 101 when dice 1 and dice 2 are same ", () => {
+    const minRange = 1;
+    const maxRange = 1;
+    expect(diceHandValueCopy(minRange, maxRange)).to.be.equal(101);
+  });
+});
+
+describe("Untestable 2: a dice game", () => {
+  test("the result ranges from 1 to 4 and from 101 to 104 if the dice are the same", () => {
+    const minRange = 1;
+    const maxRange = 4;
+    expect(diceHandValueCopy(minRange, maxRange)).to.be.oneOf([1, 2, 3, 4, 101, 102, 103, 104]);
   });
 });
